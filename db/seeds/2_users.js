a27f506ca4c6d6420559fbda646869e0a9a57cb6
annineedscoffee <char.anne.spencer@gmail.com>
2017-10-30 14:03:16 -0700

    another draft

--- db/seeds/2_users.js
+++ db/seeds/2_users.js
@@ -7,7 +7,7 @@ exports.seed = function(knex, Promise) {
       return knex('users').insert([
         {first_name: 'Char', last_name: 'Spencer', cohort_id: 1, email: "char.anne.spencer@gmail.com", password: "gforumpass1", auth_level: 2},
         {first_name: 'Trevor', last_name: 'Young', cohort_id: 1, email: "trevorsemail@email.com", password: "gforumpass1", auth_level: 2},
-        {first_name: 'Austin', last_name: 'Gohde', cohort_id: 1, email: "austinsemail@email.com", password: "gforumpass1", auth_level: 2},
+        {first_name: 'Austin', last_name: 'Gohde', cohort_id: 1, email: "austinsemail@email.com", password: "gforumpass1", auth_level: 2},         {first_name: 'Random', last_name: 'Person', cohort_id: 1, email: "randomperson@email.com", password: "gforumpass1", auth_level: 1}
       ]);
     });
 };
