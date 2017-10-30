bcb3d4f5a988a3d99564035f0cff76a0553a67d1
Austin Gohde <sphinx_985@msn.com>
2017-10-30 13:40:20 -0700

    landing page, post login, register page and create

--- knexfile.js
+++ knexfile.js
@@ -3,7 +3,7 @@ module.exports = {
       client: 'pg',
       connection: {
         database: "gForum",
-        connection: "localhost"
+        host: "localhost"
       },
       migrations: {
           directory: __dirname + '/db/migrations',
