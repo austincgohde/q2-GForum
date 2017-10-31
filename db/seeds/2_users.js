8fe4bb75f717e1cc211f22d4ffafc59e10addf8d
Austin Gohde <sphinx_985@msn.com>
2017-10-31 12:03:45 -0700

    seeded with migrations

--- db/seeds/2_users.js
+++ db/seeds/2_users.js
@@ -5,9 +5,9 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       // Inserts seed entries
       return knex('users').insert([
-        {first_name: 'Char', last_name: 'Spencer', cohort_id: 1, email: "char.anne.spencer@gmail.com", password: "gforumpass1", auth_level: 2},
-        {first_name: 'Trevor', last_name: 'Young', cohort_id: 1, email: "trevorsemail@email.com", password: "gforumpass1", auth_level: 2},
-        {first_name: 'Austin', last_name: 'Gohde', cohort_id: 1, email: "austinsemail@email.com", password: "gforumpass1", auth_level: 2}
+        {first_name: 'Charlotte-Anne', last_name: 'Spencer', cohort_id: 1, email: "char@char.com", password: "$2a$10$Bm2Wrd1JUvJxw7vLU.9JY.5fnKXYkCDxMmz9z4HgWr60ae.24kX2u", auth_level: 2},
+        {first_name: 'Trevor', last_name: 'Young', cohort_id: 1, email: "tcyoung4@asu.edu", password: "$2a$10$klNA9OK8GJcO5kQYUsPppOotHNjni0vR5BFNHv4UDbguxHXZ1Kpri", auth_level: 2},
+        {first_name: 'Austin', last_name: 'Gohde', cohort_id: 1, email: "austinsemail@email.com", password: "$2a$10$EWgQya/x9itL1Mq1306gcenhZKmEBWHc5YhVnNqu9FS3tLLAlVgDS", auth_level: 2}
       ]);
     });
 };
