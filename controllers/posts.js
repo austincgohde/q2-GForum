118c24aaa81ce3f8a0db0ba48a5714a95f599a16
annineedscoffee <char.anne.spencer@gmail.com>
2017-10-31 14:48:40 -0700

    post controllers done

--- controllers/posts.js
+++ controllers/posts.js
@@ -82,49 +82,4 @@ const encryption = require("../config/encryption.js");
          });
         },
 
-//Interestings:        
-                 /*
-
-   singlePost: function(req, res){
-     knex('posts')
-     .where('id', req.params.id)
-     .select('title', 'content', 'type.name', 'users.first_name', 'upvotes', 'downvotes')
-     .join('users', 'users.id', 'posts.user_id')
-     .join('types', 'types.id', 'posts.type_id')
-     .then((result)=>{
-       let post = result[0];
-       knex('comments')
-       .where('posts_id', req.params.id)
-       .select('content', 'upvotes', 'downvotes', 'users.first_name')
-     })
-     .join('users', 'users.id', 'comments.user_id')
-     .then((result)=>{
-       res.render('pages/post', { post: post, comments: result})
-     });
-   }, */
-
-/*
-
-     interest: function(req, res){
-       knex('posts')
-         .then((postsResults)=>{
-           knex('users')
-             .where('user_id', req.params.id)
-               .then((usersResults)=>{
-                 .knex('types')
-                   .where('type_id', req.params.id)
-                   .then((typesResults)=>{
-                     .knex('comments')
-                       .where('user_id',req.params.id)
-                         .then((commentsResults)=>{
-                           res.render('/interestings', {posts: postResults[0], users: usersResults, types: typesResults, comments: commentsResults});
-                         });
-                       });
-                     });
-                   });
-                   .catch((err) => {
-                     console.error(err)
-                   });
-                 }*/
-
  };
