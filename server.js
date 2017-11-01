4d710aa3c91f5597c62d6952bc9f9358762f1627
Austin Gohde <sphinx_985@msn.com>
2017-11-01 15:29:16 -0700

    fixs

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
 const express = require("express");
 const path = require("path");
 const app = express();
+const logger = require("morgan");
 const bodyParser = require('body-parser');
 const port = process.env.PORT || 8000;
 app.use(bodyParser.json({ extended: true }));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
+app.use(logger("dev"));
 
 require('./config/sessions.js')(app);
 
 app.set('view engine', 'ejs');
+
 
 var routes_setter = require('./config/routes.js');
 routes_setter(app);
