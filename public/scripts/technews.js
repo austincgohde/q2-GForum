27c2ed90545652586c97062d9cc698ae8bab953c
annineedscoffee <char.anne.spencer@gmail.com>
2017-11-01 14:54:02 -0700

    jquery stuff

--- public/scripts/technews.js
+++ public/scripts/technews.js
@@ -5,21 +5,21 @@ $.get((' https://newsapi.org/v1/articles?source=techcrunch&sortBy=latest&apiKey=
   $('#techNewsAuthor').text(data.articles[0].author);
   $('#techNewsDesc').text(data.articles[0].description);
   $('#Date').text(data.articles[0].publishedAt);
-  $('#link').append(data.articles[0].url);
+  $('#newsLink').append(data.articles[0].url);
   $('#imageLink').append(data.articles[0].urlToImage);
 
   $('#techNewsTitle2').text(data.articles[1].title);
   $('#techNewsAuthor2').text(data.articles[1].author);
   $('#techNewsDesc2').text(data.articles[1].description);
   $('#Date2').text(data.articles[1].publishedAt);
-  $('#link2').append(data.articles[1].url);
+  $('#newsLink2').append(data.articles[1].url);
   $('#imageLink2').append(data.articles[1].urlToImage);
 
   $('#techNewsTitle3').text(data.articles[2].title);
   $('#techNewsAuthor3').text(data.articles[2].author);
   $('#techNewsDesc3').text(data.articles[2].description);
   $('#Date3').text(data.articles[2].publishedAt);
-  $('#link3').append(data.articles[2].url);
+  $('#newsLink3').append(data.articles[2].url);
   $('#imageLink3').append(data.articles[2].urlToImage);
 });
 
