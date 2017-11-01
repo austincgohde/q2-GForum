8fc34ff6b439684dcf4f2179d3e8d868d3e15ff0
trevoryoungPHX <tcyoung4@asu.edu>
2017-11-01 15:29:28 -0700

    Removed css links

--- public/scripts/technews.js
+++ public/scripts/technews.js
@@ -1,25 +1,25 @@
 $(document).ready(function(){
 // alert();
-$.get((' https://newsapi.org/v1/articles?source=techcrunch&sortBy=latest&apiKey=2dfc41eabdaa4c38b740b79835fcb3f2'), function(data){
+$.get((' https://newsapi.org/v1/articles?source=techcrunch&sortBy=latest&apiKey={2dfc41eabdaa4c38b740b79835fcb3f2}'), function(data){
   $('#techNewsTitle').text(data.articles[0].title);
   $('#techNewsAuthor').text(data.articles[0].author);
   $('#techNewsDesc').text(data.articles[0].description);
   $('#Date').text(data.articles[0].publishedAt);
-  $('#newsLink').append(data.articles[0].url);
-  let imageLink = data.articles[0].urltoImage;
+  $('#link').append(data.articles[0].url);
+  $('#imageLink').append(data.articles[0].urlToImage);
 
   $('#techNewsTitle2').text(data.articles[1].title);
   $('#techNewsAuthor2').text(data.articles[1].author);
   $('#techNewsDesc2').text(data.articles[1].description);
   $('#Date2').text(data.articles[1].publishedAt);
-  $('#newsLink2').append(data.articles[1].url);
+  $('#link2').append(data.articles[1].url);
   $('#imageLink2').append(data.articles[1].urlToImage);
 
   $('#techNewsTitle3').text(data.articles[2].title);
   $('#techNewsAuthor3').text(data.articles[2].author);
   $('#techNewsDesc3').text(data.articles[2].description);
   $('#Date3').text(data.articles[2].publishedAt);
-  $('#newsLink3').append(data.articles[2].url);
+  $('#link3').append(data.articles[2].url);
   $('#imageLink3').append(data.articles[2].urlToImage);
 });
 
