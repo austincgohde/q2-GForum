c423f9ff0d87f058dff3bb6c8a02ddc5a42ffa8c
annineedscoffee <char.anne.spencer@gmail.com>
2017-11-02 16:39:27 -0700

    somewhat seeded

--- db/seeds/3_types.js
+++ db/seeds/3_types.js
@@ -17,13 +17,13 @@ exports.seed = function(knex, Promise) {
         {name: 'React', image: 'react.png'},
         {name: 'Redux', image: 'redux.png'},
         {name: 'Angular', image: 'angular.png'},
-        {name: 'Vue', image: 'vue.png'},13
-        {name: 'Java', image: 'java.png'},14
-        {name: 'C#', image: 'csharp.png'},15
-        {name: '.Net', image: 'net.png'},16
+        {name: 'Vue', image: 'vue.png'},
+        {name: 'Java', image: 'java.png'},
+        {name: 'C#', image: 'csharp.png'},
+        {name: '.Net', image: 'net.png'},
         {name: 'Python', image: 'python.png'},
-        {name: 'Django', image: 'django.png'},18
-        {name: 'Computer_Science', image: 'cs.png'}19
+        {name: 'Django', image: 'django.png'},
+        {name: 'Computer_Science', image: 'cs.png'}
       ]);
     });
 };
--- db/seeds/6_comments.js
+++ db/seeds/6_comments.js
@@ -5,7 +5,95 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       // Inserts seed entries
       return knex('comments').insert([
-        {user_id: 1, post_id: 1, content: "cool website!", upvote: 0, downvote: 0}
+        {user_id: 1, post_id: 1, content: "cool website!", upvote: 0, downvote: 0},
+        {user_id: 1, post_id: 1, content: "nifty website!", upvote: 0, downvote: 0},
+        {user_id: 1, post_id: 1, content: "awesome website!", upvote: 0, downvote: 0},
+        {user_id: 1, post_id: 1, content: "cool website!", upvote: 0, downvote: 0},
+
+        {user_id: 1, post_id: 2, content: "comment comment comment", upvote: 0, downvote: 0},
+        {user_id: 1, post_id: 2, content: "blah blah blah - this is my comment", upvote: 0, downvote: 0},
+        {user_id: 1, post_id: 2, content: "cool website!", upvote: 0, downvote: 0},
+        {user_id: 1, post_id: 2, content: "cool website!", upvote: 0, downvote: 0},
+
+        {user_id: 1, post_id: 3, content: "I need help with this too", upvote: 0, downvote: 0},
+        {user_id: 1, post_id: 3, content: "cool website!", upvote: 0, downvote: 0},
+        {user_id: 1, post_id: 3, content: "I can help wit this!", upvote: 0, downvote: 0},
+        {user_id: 1, post_id: 3, content: "cool website!", upvote: 0, downvote: 0},
+
+        {user_id: 1, post_id: 4, content: "Can you show me more about this?", upvote: 0, downvote: 0},
+        {user_id: 1, post_id: 4, content: "I have another solution", upvote: 0, downvote: 0},
+        {user_id: 1, post_id: 4, content: "cool website!", upvote: 0, downvote: 0},
+        {user_id: 1, post_id: 4, content: "cool website!", upvote: 0, downvote: 0},
+
+        {user_id: 1, post_id: 5, content: "I can solve this problem", upvote: 0, downvote: 0},
+        {user_id: 1, post_id: 5, content: "cool design!", upvote: 0, downvote: 0},
+        {user_id: 1, post_id: 5, content: "cool website!", upvote: 0, downvote: 0},
+        {user_id: 1, post_id: 5, content: "cool website!", upvote: 0, downvote: 0},
+
+        {user_id: 1, post_id: 6, content: "I can fix this", upvote: 0, downvote: 0},
+        {user_id: 1, post_id: 6, content: "I want help with this too!", upvote: 0, downvote: 0},
+        {user_id: 1, post_id: 6, content: "cool website!", upvote: 0, downvote: 0},
+        {user_id: 1, post_id: 6, content: "cool website!", upvote: 0, downvote: 0},
+
+        {user_id: 1, post_id: 7, content: "I'm not sure how to solve that!", upvote: 0, downvote: 0},
+        {user_id: 1, post_id: 7, content: "cool website!", upvote: 0, downvote: 0},
+        {user_id: 1, post_id: 7, content: "cool website!", upvote: 0, downvote: 0},
+        {user_id: 1, post_id: 7, content: "cool website!", upvote: 0, downvote: 0},
+
+        {user_id: 1, post_id: 8, content: "I'm confused!", upvote: 0, downvote: 0},
+        {user_id: 1, post_id: 8, content: "cool website!", upvote: 0, downvote: 0},
+        {user_id: 1, post_id: 8, content: "cool website!", upvote: 0, downvote: 0},
+        {user_id: 1, post_id: 8, content: "cool website!", upvote: 0, downvote: 0},
+
+        {user_id: 1, post_id: 9, content: "cool website!", upvote: 0, downvote: 0},
+        {user_id: 1, post_id: 9, content: "cool website!", upvote: 0, downvote: 0},
+        {user_id: 1, post_id: 9, content: "cool website!", upvote: 0, downvote: 0},
+        {user_id: 1, post_id: 9, content: "cool website!", upvote: 0, downvote: 0},
+
+        {user_id: 1, post_id: 10, content: "cool website!", upvote: 0, downvote: 0},
+        {user_id: 1, post_id: 10, content: "nifty website!", upvote: 0, downvote: 0},
+        {user_id: 1, post_id: 11, content: "awesome website!", upvote: 0, downvote: 0},
+        {user_id: 1, post_id: 11, content: "cool website!", upvote: 0, downvote: 0},
+
+        {user_id: 1, post_id: 12, content: "comment comment comment", upvote: 0, downvote: 0},
+        {user_id: 1, post_id: 12, content: "blah blah blah - this is my comment", upvote: 0, downvote: 0},
+        {user_id: 1, post_id: 12, content: "cool website!", upvote: 0, downvote: 0},
+        {user_id: 1, post_id: 12, content: "cool website!", upvote: 0, downvote: 0},
+
+        {user_id: 1, post_id: 13, content: "I need help with this too", upvote: 0, downvote: 0},
+        {user_id: 1, post_id: 13, content: "cool website!", upvote: 0, downvote: 0},
+        {user_id: 1, post_id: 13, content: "I can help wit this!", upvote: 0, downvote: 0},
+        {user_id: 1, post_id: 13, content: "cool website!", upvote: 0, downvote: 0},
+
+        {user_id: 1, post_id: 14, content: "Can you show me more about this?", upvote: 0, downvote: 0},
+        {user_id: 1, post_id: 14, content: "I have another solution", upvote: 0, downvote: 0},
+        {user_id: 1, post_id: 14, content: "cool website!", upvote: 0, downvote: 0},
+        {user_id: 1, post_id: 14, content: "cool website!", upvote: 0, downvote: 0},
+
+        {user_id: 1, post_id: 15, content: "I can solve this problem", upvote: 0, downvote: 0},
+        {user_id: 1, post_id: 15, content: "cool design!", upvote: 0, downvote: 0},
+        {user_id: 1, post_id: 15, content: "cool website!", upvote: 0, downvote: 0},
+        {user_id: 1, post_id: 15, content: "cool website!", upvote: 0, downvote: 0},
+
+        {user_id: 1, post_id: 16, content: "I can fix this", upvote: 0, downvote: 0},
+        {user_id: 1, post_id: 16, content: "I want help with this too!", upvote: 0, downvote: 0},
+        {user_id: 1, post_id: 16, content: "cool website!", upvote: 0, downvote: 0},
+        {user_id: 1, post_id: 16, content: "cool website!", upvote: 0, downvote: 0},
+
+        {user_id: 1, post_id: 17, content: "I'm not sure how to solve that!", upvote: 0, downvote: 0},
+        {user_id: 1, post_id: 17, content: "cool website!", upvote: 0, downvote: 0},
+        {user_id: 1, post_id: 17, content: "cool website!", upvote: 0, downvote: 0},
+        {user_id: 1, post_id: 17, content: "cool website!", upvote: 0, downvote: 0},
+
+        {user_id: 1, post_id: 18, content: "I'm confused!", upvote: 0, downvote: 0},
+        {user_id: 1, post_id: 18, content: "cool website!", upvote: 0, downvote: 0},
+        {user_id: 1, post_id: 18, content: "cool website!", upvote: 0, downvote: 0},
+        {user_id: 1, post_id: 18, content: "cool website!", upvote: 0, downvote: 0},
+
+        {user_id: 1, post_id: 19, content: "cool website!", upvote: 0, downvote: 0},
+        {user_id: 1, post_id: 19, content: "cool website!", upvote: 0, downvote: 0},
+        {user_id: 1, post_id: 19, content: "cool website!", upvote: 0, downvote: 0},
+        {user_id: 1, post_id: 19, content: "cool website!", upvote: 0, downvote: 0},
       ]);
     });
 };
