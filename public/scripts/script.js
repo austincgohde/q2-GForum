1c6c5bbe368296a031a62c7c31b4b7f99c023d5a
trevoryoungPHX <tcyoung4@asu.edu>
2017-11-02 09:30:51 -0700

    Fixed news links

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -9,22 +9,21 @@ $.get((' https://newsapi.org/v1/articles?source=techcrunch&sortBy=latest&apiKey=
   $('#techNewsAuthor').text(data.articles[0].author);
   $('#techNewsDesc').text(data.articles[0].description);
   $('#Date').text(data.articles[0].publishedAt);
-  $('#newsLink').append("href", data.articles[0].url);
+  $('#newsLink').attr("href", data.articles[0].url);
   $("#imageLink").attr("src", data.articles[0].urlToImage);
-
 
   $('#techNewsTitle2').text(data.articles[1].title);
   $('#techNewsAuthor2').text(data.articles[1].author);
   $('#techNewsDesc2').text(data.articles[1].description);
   $('#Date2').text(data.articles[1].publishedAt);
-  $('#newsLink2').append("href", data.articles[1].url);
+  $('#newsLink2').attr("href", data.articles[1].url);
   $("#imageLink2").attr("src", data.articles[1].urlToImage);
 
   $('#techNewsTitle3').text(data.articles[2].title);
   $('#techNewsAuthor3').text(data.articles[2].author);
   $('#techNewsDesc3').text(data.articles[2].description);
   $('#Date3').text(data.articles[2].publishedAt);
-  $('#newsLink3').append("href", data.articles[2].url);
+  $('#newsLink3').attr("href", data.articles[2].url);
   $("#imageLink3").attr("src", data.articles[2].urlToImage);
 });
 
