16bd0f40462c827e065be8638b9bbcf23fe5d419
trevoryoungPHX <tcyoung4@asu.edu>
2017-11-02 19:45:33 -0700

    Updated CSS

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -41,4 +41,16 @@ window.onclick = function(event) {
         modal.style.display = "none";
     }
 }
-});
+
+$("#slideshow > div:gt(0)").hide();
+
+setInterval(function() {
+  $('#slideshow > div:first')
+    .fadeOut(1000)
+    .next()
+    .fadeIn(1000)
+    .end()
+    .appendTo('#slideshow');
+},  3000);
+
+}
