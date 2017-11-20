79b75bbaae32d26a842e4f358ac409d503760646
Austin Gohde <sphinx_985@msn.com>
2017-11-20 16:04:32 -0700

    api fix

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 // alert();
-$.get(('http://quotes.rest/qod.json?category=students'), function(data){
+$.get(('https://quotes.rest/qod.json?category=students'), function(data){
   $('#quoteText').text(' "' + data.contents.quotes[0].quote + '"' + ' - ' + data.contents.quotes[0].author);
 });
 
